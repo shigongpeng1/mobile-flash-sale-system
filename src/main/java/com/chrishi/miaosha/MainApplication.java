31333e988d04ff892bdd8d36e9c56fe3807c61fd
shigongpeng <503484839@qq.com>
2019-10-20 16:19:27 +0800

    [*]war包

--- src/main/java/com/chrishi/miaosha/MainApplication.java
+++ src/main/java/com/chrishi/miaosha/MainApplication.java
@@ -6,13 +6,13 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MainApplication /*extends SpringBootServletInitializer*/{
+public class MainApplication extends SpringBootServletInitializer{
 
     public static void main(String[] args){
         SpringApplication.run(MainApplication.class,args);
     }
 
-    /*protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
        return builder.sources(MainApplication.class);
-    }*/
+    }
 }
